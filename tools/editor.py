1dd82dd800dd0f38ec34a2e06f3ffaa67e61a3d7
Aaron Olkin <aaronolkin@gmail.com>
2017-06-18 01:36:22 -0400

    Changed working sample room, editor handles loading and saving better

--- tools/editor.py
+++ tools/editor.py
@@ -4,9 +4,11 @@ import sys
 
 from toollib import pb, get
 
+fn = sys.argv[1] if len(sys.argv) > 1 else get.filename("rooms")
+
 level = pb.Level()
 try:
-    fd = open(sys.argv[1], "rb")
+    fd = open(fn, "rb")
     level.ParseFromString(fd.read())
     fd.close()
     parsed = True
@@ -34,6 +36,6 @@ print("\nPLAYER START POSITION")
 level.starts[0].x = get.number("x")
 level.starts[0].y = get.number("y")
 
-fd = open(sys.argv[1], "wb")
+fd = open(fn, "wb")
 fd.write(level.SerializeToString())
 fd.close()
